4ce708ba6778aa777f724f50b520c60d68d06e4b
ahmad gholamnia <a_gholamnia@yahoo.com>
2019-05-20 00:22:27 +0430

    Updates

---
+++ precache-manifest.41ff3d2ff1614745a65265d864b23346.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff87fe4c5e5f52eba889ebf8f03fb42",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "1bf6f11afe6008116e73",
+    "url": "/contactmanager/static/css/2.be9e7677.chunk.css"
+  },
+  {
+    "revision": "f22416e015000cb15957",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "1bf6f11afe6008116e73",
+    "url": "/contactmanager/static/js/2.9313f830.chunk.js"
+  },
+  {
+    "revision": "f22416e015000cb15957",
+    "url": "/contactmanager/static/js/main.baa05061.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/contactmanager/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/contactmanager/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/contactmanager/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/contactmanager/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/contactmanager/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
